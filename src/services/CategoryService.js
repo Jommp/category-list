487c4b8ae89a453c0802ed3f73004931d00732db
Jommp <jommp.96@gmail.com>
2022-01-21 21:46:48 -0500

    Config env variables

--- src/services/CategoryService.js
+++ src/services/CategoryService.js
@@ -5,10 +5,10 @@ export default {
 }
 
 async function apiCall(url, method = 'GET') {
-  const response = await fetch('https://apitesting.plerk.io/v2/' + url, {
+  const response = await fetch(process.env.VUE_APP_INTERNAL_API_BASE_URL + url, {
     method: method,
     headers: {
-      'Authorization': 'Bearer 5bc95bf034d900548243a59e2296bd683729bd75057879fd0f877d3adc7d1db6bedbfccb47aca04e44ef28adf4c3e9e72afe2f2b295b3bf08e2a47ec75f9607d',
+      'Authorization': 'Bearer ' + process.env.VUE_APP_API_KEY,
       'Accept': 'application/json, text/plain, */*'
     }
   })
